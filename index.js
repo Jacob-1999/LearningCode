e2c99cca99ee0df6265e0d8602ce5abe317f6b45
J-Daniel-1 <goldenfreddyv2@gmail.com>
2022-03-24 22:55:56 -0400

    Welcome message and channel

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ require("dotenv").config()
 const client = new Discord.Client({
   intents: [
     "GUILDS",
-    "GUILD_MESSAGES"
+    "GUILD_MESSAGES",
+    "GUILD_MEMBERS"
   ]
 })
 
@@ -16,6 +17,12 @@ client.on("messageCreate", (message) => {
   if (message.content == "ping"){
     message.reply("pong")
   }
+})
+
+const welcomeChannelID = "956721846240301077"
+
+client.on("guildMemberAdd", (member) => {
+  member.guild.channels.cache.get(welcomeChannelID).send(`(${member.id}) **${member.user.tag}** has joined.`)
 })
 
 client.login(process.env.TOKEN)
